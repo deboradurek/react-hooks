fd7efdb86dd6e2b3fb28419f88bab7acbcebfbfd
Debora Durek <debora.durek@gmail.com>
2022-08-30 19:42:54 -0600

    useEffect: persistent state [Extra Credit] flexible localStorage hook

--- src/exercise/useLocalStorageState.js
+++ src/exercise/useLocalStorageState.js
@@ -1,10 +1,29 @@
 import React from "react"
 
 export const useLocalStorageState = (key, defaultValue = '') => {
-    const [name, setName] = React.useState(() => window.localStorage.getItem(key) ?? defaultValue)
+
+    const [name, setName] = React.useState(
+      // Lazy initialization
+      () => {
+        const valueInLocalStorage = window.localStorage.getItem(key)
+        if (valueInLocalStorage) {
+          return JSON.parse(valueInLocalStorage)
+        }
+        // Adding the ability to set the default value to a function
+        // To avoid a computationally expensive function being created at every render
+        return typeof defaultValue === 'function' ? defaultValue() : defaultValue
+      })
+
+      // Keep track of the previous key value through the entire lifecycle of the component:
+      const prevKeyRef = React.useRef(key)
 
     React.useEffect(() => {
-      window.localStorage.setItem(key, name)
+      const prevKey = prevKeyRef.current
+      if (prevKey !== key) {
+        window.localStorage.removeItem(key)
+      }
+      prevKeyRef.current = key
+      window.localStorage.setItem(key, JSON.stringify(name))
     }, [key, name])
 
     function handleChange(event) {
